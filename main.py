b9869b06f075dfb1fd5c05b31b6b647da7fb5c6d
eric brandt <ericbrandt@gmx.de>
2022-05-01 20:08:55 +0200

    mqtt publish subscribe

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import awsiot.greengrasscoreipc.client as client
 from awsiot.greengrasscoreipc.model import (
     IoTCoreMessage,
     QOS,
-    SubscribeToIoTCoreRequest,
-    PublishToIoTCoreRequest
+    SubscribeToIoTCoreRequest
 )
 
 TIMEOUT = 10
@@ -19,13 +18,10 @@ class StreamHandler(client.SubscribeToIoTCoreStreamHandler):
         super().__init__()
 
     def on_stream_event(self, event: IoTCoreMessage) -> None:
-        print("fuck")
-        with open("/test.txt", "wb") as f:
-            f.write("gs")
-            f.close()
         try:
             message = str(event.message.payload, "utf-8")
             topic_name = event.message.topic_name
+            # Handle message.
             print("message received")
         except:
             traceback.print_exc()
@@ -43,16 +39,17 @@ topic = "my/topic"
 qos = QOS.AT_MOST_ONCE
 
 request = SubscribeToIoTCoreRequest()
-request.topic_name = "my/topics"
+request.topic_name = topic
 request.qos = qos
 handler = StreamHandler()
 operation = ipc_client.new_subscribe_to_iot_core(handler)
 future = operation.activate(request)
+print("subscribed")
 future.result(TIMEOUT)
-
+print("result wait")
 # Keep the main thread alive, or the process will exit.
 while True:
-    #time.sleep(10)
-    pass
+    time.sleep(10)
                   
 # To stop subscribing, close the operation stream.
+operation.close()
